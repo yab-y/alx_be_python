8c9d0d5b30fedda605f9c43ca875f7308215af9e
Yeabsira Wondwossen <yeabwondu48@gmail.com>
2025-06-25 13:33:33 +0300

    fixed i to interest

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,5 @@
 principal = 1000
 rate = 0.05
 time = 3
-I = principal * rate * time
-print('The simple interest is:', I)
+interest = principal * rate * time
+print('The simple interest is:', interest)
