2a4e9a4664189299031084b263dcaf7747103502
Yeabsira Wondwossen <yeabwondu48@gmail.com>
2025-06-26 14:00:01 +0300

    python code for weather recommendation program

---
+++ control_flow/weather_advice.py
@@ -0,0 +1,10 @@
+weather = input("what's the weather like today? (sunny/rainy/cold): ")
+if weather == "sunny":
+    clothing_recommmendation = "Wear a t-shirt and sunglasses."
+elif weather == "rainy":
+    clothing_recommmendation = "Don't forget your umbrella and a raincoat."
+elif weather == "cold":
+    clothing_recommmendation = "Make sure to wear a warm coat and a scarf."
+else:
+    clothing_recommmendation = "Sorry, I don't have recommmendations for this weather."
+print(clothing_recommmendation)
